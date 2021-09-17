87ccfc069ad869f7ec93d8ef4d66326975471bad
JeppeKorgaard98 <90270219+JeppeKorgaard98@users.noreply.github.com>
2021-09-17 13:54:07 +0200

    Hurtig (#3)
    * Created student class and added tests
    
    * quick fix
    
    Co-authored-by: jeppekorgaard <korg@itu.dk>

--- Assignment2/Student.cs
+++ Assignment2/Student.cs
@@ -1,42 +1,45 @@
 using System;
 
-public class Student
+namespace Assignment2
 {
-    public int id { get; }
-    public string givenName { get; set; }
-    public string surName { get; set; }
-    public Status status { get; }
-    public DateTime startDate { get; set; }
-    public DateTime endDate { get; set; }
-    public DateTime graduationDate { get; set; }
-
-    public Student(int id, string GivenName, string Surname, DateTime startDate)
+    public class Student
     {
-        this.id = id;
-        this.givenName = GivenName;
-        this.surName = Surname;
-        this.startDate = startDate;
-        this.graduationDate = startDate.AddYears(3);
+        public int id { get; }
+        public string givenName { get; set; }
+        public string surName { get; set; }
+        public Status status { get; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        public DateTime graduationDate { get; set; }
 
-        if (DateTime.Now > graduationDate)
+        public Student(int id, string GivenName, string Surname, DateTime startDate)
         {
-            this.status = Status.Graduated;
+            this.id = id;
+            this.givenName = GivenName;
+            this.surName = Surname;
+            this.startDate = startDate;
+            this.graduationDate = startDate.AddYears(3);
+
+            if (DateTime.Now > graduationDate)
+            {
+                this.status = Status.Graduated;
+            }
+            else if (DateTime.Now < startDate)
+            {
+                this.status = Status.New;
+            }
+            else
+            {
+                this.status = Status.Active;
+            }
         }
-        else if (DateTime.Now < startDate)
+
+
+
+
+        public static void Main(string[] args)
         {
-            this.status = Status.New;
+
         }
-        else
-        {
-            this.status = Status.Active;
-        }
-    }
-
-
-
-
-    public static void main(string[] args)
-    {
-
     }
 }
